6f321e1b03ef63863bf26df06f9757325078ad9f
Erik Verbruggen <erik.verbruggen@nokia.com>
2011-08-05 11:32:45 +0200

    Fixed UnsavedFiles generation.

--- src/plugins/cpptools/clangutils.cpp
+++ src/plugins/cpptools/clangutils.cpp
@@ -1,20 +1,33 @@
 #include "clangutils.h"
 
+#include <editormanager/editormanager.h>
+#include <editormanager/ieditor.h>
+#include <coreplugin/icore.h>
+
 #include <QFile>
+#include <QSet>
+#include <QString>
 
 using namespace Clang;
+using namespace Core;
 
 CPPTOOLS_EXPORT ClangWrapper::UnsavedFiles CppTools::ClangUtils::createUnsavedFiles(const CPlusPlus::CppModelManagerInterface::WorkingCopy &workingCopy)
 {
     // TODO: change the modelmanager to hold one working copy, and amend it every time we ask for one.
     // TODO: Reason: the UnsavedFile needs a QByteArray.
 
+    ICore *core = ICore::instance(); // FIXME
+    QSet<QString> openFiles;
+    foreach (IEditor *editor, core->editorManager()->openedEditors())
+        openFiles.insert(editor->file()->fileName());
+
     QList<ClangWrapper::UnsavedFile> result;
     QHashIterator<QString, QPair<QString, unsigned> > wcIter = workingCopy.iterator();
     while (wcIter.hasNext()) {
         wcIter.next();
-        if (QFile(wcIter.key()).exists())
-            result.append(ClangWrapper::UnsavedFile(wcIter.key(), wcIter.value().first.toUtf8()));
+        const QString &fileName = wcIter.key();
+        if (openFiles.contains(fileName) && QFile(fileName).exists())
+            result.append(ClangWrapper::UnsavedFile(fileName, wcIter.value().first.toUtf8()));
     }
 
     return result;
